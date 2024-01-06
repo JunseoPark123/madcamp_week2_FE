af191582ce69512b66935881a0dc6b500877c726
JunseoPark123 <pjunseo7965@gmail.com>
2024-01-06 14:24:26 +0900

    logindesgin v1

--- app/src/main/java/com/example/madcamp_week2_fe/Interfaces/UserApiService.kt
+++
@@ -1,4 +0,0 @@
-package com.example.madcamp_week2_fe.Interfaces
-
-interface UserApiService {
-}
--- app/src/main/java/com/example/madcamp_week2_fe/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/madcamp_week2_fe/LoginRegisterActivity.kt
@@ -1,11 +1,25 @@
 package com.example.madcamp_week2_fe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_register)
+
+        val btnLogin : Button = findViewById(R.id.button_login)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this@LoginRegisterActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnRegister : Button = findViewById(R.id.button_create_account)
+        btnRegister.setOnClickListener {
+            val intent = Intent(this@LoginRegisterActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
